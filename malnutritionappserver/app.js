35589d04974524bf8eccc2d16e4820f7f6027177
Bowen Jin <bowen.jin@vanderbilt.edu>
2016-12-03 19:19:38 -0600

    in the process of finishing server

--- malnutritionappserver/app.js
+++ malnutritionappserver/app.js
@@ -7,15 +7,18 @@ var jsonlint = require("jsonlint");
 var fs = require('fs');
 var path = require('path');
 
+var server_password = "12345";
+
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
 var port = 3000;
-
 app.listen(port, function(){
     //Callback triggered when server is successfully listening. Hurray!
+
     console.log("Server listening on: http://localhost:%s", port);
 });
 
@@ -30,24 +33,34 @@ app.get('/', function (req, res) {
     });
 })
 
-app.post('/', function(req, res) {
-    var username = req.body.username,
-        password = req.body.password;
+app.post('/login', function(req, res) {
+    console.log("login called!")
+    // var username = req.body.username,
+    var server_password = "12345";
+    var password = req.body.password;
+
 
     var filePath = path.join(__dirname, 'string.json');
-
-    fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
-        if (!err){
-            // console.log('received data: ' + data);
-            res.send(data);
-        }else{
-            console.log(err);
-        }
-
-    });
+    if(password == server_password) {
+        console.log("passwords match!")
+        fs.readFile(filePath, {encoding: 'utf-8'}, function (err, data) {
+            if (!err) {
+                console.log('received data: ' + data);
+                res.send({data: data, error: null});
+            } else {
+                console.log(err);
+                res.send({data: null, error: err});
+            }
+        });
+    }
+    else{
+        console.log("passwords dont match!")
+        res.send({data: null, error: "invalid password"});
+    }
 });
 
 app.post('/validate', function(req, res){
+    console.log("validate called!")
     var json = req.body.json;
     console.log(json);
     try {
@@ -60,8 +73,33 @@ app.post('/validate', function(req, res){
         // console.log(e);
         // console.log(e.message)
     }
-
 });
+
+app.post('/update_json', function(req, res){
+    console.log("update json called!")
+    var json = req.body.json;
+    var password = req.body.password;
+
+    var password = req.body.password;
+    var filePath = path.join(__dirname, 'string.json');
+
+    console.log(json);
+    try {
+        var result = jsonlint.parse(json);
+
+        console.log(result)
+        fs.writeFile (filePath, json, function(err) {
+            if (err) throw err;
+            console.log('successfully wrote to file complete');
+            res.send({data: null, error: null});
+        });
+    } catch(e) {
+        console.log("Error caught!");
+        res.send({data: null, error: e.message});
+        // console.log(e);
+        // console.log(e.message)
+    }
+})
 
 // fs.readFile(filePath, 'utf8', function (err, data) {
 //     if (err) throw err;
