a36d87d86da5021e14469febbc10989a18494523
Bowen Jin <bowen.jin@vanderbilt.edu>
2016-12-04 00:15:35 -0600

    Added validation, authentication, and get_json methods, made the API more modular, also added auth_key so there can be login sessions

--- malnutritionappserver/app.js
+++ malnutritionappserver/app.js
@@ -7,15 +7,17 @@ var jsonlint = require("jsonlint");
 var fs = require('fs');
 var path = require('path');
 
-var server_password = "12345";
-
-
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
 var port = 3000;
+
+var auth_key_arr = [];
+
+var server_password = "12345";
+
 app.listen(port, function(){
     //Callback triggered when server is successfully listening. Hurray!
 
@@ -33,73 +35,114 @@ app.get('/', function (req, res) {
     });
 })
 
+app.post('/get_json', function(req, res){
+    var filePath = path.join(__dirname, 'main.html');
+    fs.readFile(filePath, {encoding: 'utf-8'}, function (err, data) {
+        if (!err) {
+            console.log("json sent to client!");
+            res.send({data: {json: data}, error: null});
+        } else {
+            console.log(err);
+            res.send({data: null, error: err});
+        }
+    });
+})
+
 app.post('/login', function(req, res) {
     console.log("login called!")
-    // var username = req.body.username,
-    var server_password = "12345";
+    // var username = req.body.username
     var password = req.body.password;
 
-
-    var filePath = path.join(__dirname, 'string.json');
     if(password == server_password) {
         console.log("passwords match!")
-        fs.readFile(filePath, {encoding: 'utf-8'}, function (err, data) {
-            if (!err) {
-                console.log('received data: ' + data);
-                res.send({data: data, error: null});
-            } else {
-                console.log(err);
-                res.send({data: null, error: err});
+        console.log('received data: ' + data);
+        var auth_key = generateKey(24);
+        auth_key_arr.append(auth_key);
+        //set a timer to remove the key after a certain amount of time
+        setTimeout(function(){
+            var index = auth_key_arr.indexOf(auth_key);
+            if (index > -1) {
+                auth_key_arr.splice(index, 1);
             }
-        });
+        }, 1800000)
+        res.send({data: {auth_key: auth_key}, error: null});
     }
     else{
-        console.log("passwords dont match!")
+        console.log("passwords don't match!")
         res.send({data: null, error: "invalid password"});
     }
 });
 
+app.post('/authenticate', function(req, res) {
+    console.log("login called!")
+    // var username = req.body.username,
+    var key = req.body.auth_key;
+    if(auth_key_arr.indexOf(key) != -1) {
+        console.log("authentication successful!")
+        res.send({data: null, error: null});
+    }
+    else{
+        console.log("invalid auth key!")
+        res.send({data: null, error: "invalid auth key"});
+    }
+});
+
+
+
 app.post('/validate', function(req, res){
     console.log("validate called!")
+    var auth_key = req.body.auth_key
     var json = req.body.json;
     console.log(json);
-    try {
-        var result = jsonlint.parse(json);
-        console.log(result)
-        res.send({data: result, error: null});
-    } catch(e) {
-        console.log("Error caught!");
-        res.send({data: null, error: e.message});
-        // console.log(e);
-        // console.log(e.message)
+    if(auth_key_arr.indexOf(key) != -1) {
+        console.log("authentication successful!")
+        try {
+            var result = jsonlint.parse(json);
+            console.log(result)
+            res.send({data: null, error: null});
+        } catch(e) {
+            console.log("Error caught!");
+            res.send({data: null, error: e.message});
+            // console.log(e);
+            // console.log(e.message)
+        }
+    }
+    else{
+        console.log("invalid auth key!")
+        res.send({data: null, error: "invalid auth key"});
     }
 });
 
 app.post('/update_json', function(req, res){
     console.log("update json called!")
     var json = req.body.json;
-    var password = req.body.password;
-
-    var password = req.body.password;
+    var auth_key = req.body.auth_key;
     var filePath = path.join(__dirname, 'string.json');
-
     console.log(json);
-    try {
-        var result = jsonlint.parse(json);
-
-        console.log(result)
-        fs.writeFile (filePath, json, function(err) {
-            if (err) throw err;
-            console.log('successfully wrote to file complete');
-            res.send({data: null, error: null});
-        });
-    } catch(e) {
-        console.log("Error caught!");
-        res.send({data: null, error: e.message});
-        // console.log(e);
-        // console.log(e.message)
+    if(auth_key_arr.indexOf(key) != -1) {
+        try {
+            var result = jsonlint.parse(json);
+            //if parse is unsuccessful then it will throw an error and go to catch block
+            fs.writeFile (filePath, json, function(err) {
+                if (err) throw err;
+                console.log('successfully wrote to file complete');
+                res.send({data: null, error: null});
+            });
+        } catch(e) {
+            console.log("Error caught!");
+            res.send({data: null, error: e.message});
+        }
     }
-})
+    else{
+        console.log("invalid auth key!")
+        res.send({data: null, error: "invalid auth key"});
+    }
+});
+
+function generateKey(length){
+    return Math.random().toString(36).substring(length);
+}
+
 
 // fs.readFile(filePath, 'utf8', function (err, data) {
 //     if (err) throw err;
