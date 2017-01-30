6edd2c3a6cf61670a823df98728accc42da93c9e
Jason Xiong <jason.j.xiong@gmail.com>
2017-01-30 15:42:36 -0600

    .

--- darealserver2017/src/lib/index.js
+++ darealserver2017/src/lib/index.js
@@ -3,13 +3,20 @@ let mongoose = require("mongoose");
 let auth = require("./auth");
 let bodyParser = require("body-parser");
 
+
+// TODO plz change plz
+const APP_SECRET = "please change this plz";
+const ACCESS_TOKEN_OPTS = {
+  expiresIn: 60 * 60  // 1 hour
+};
 mongoose.connect("mongodb://localhost:33701/malnutrition");
 
 let app = express();
 app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use("/auth", auth.router);
+console.log(auth);
+app.use("/auth", auth.createRouter(APP_SECRET, ACCESS_TOKEN_OPTS));
 
 app.listen(3000, function() {
   console.log("app listening");
