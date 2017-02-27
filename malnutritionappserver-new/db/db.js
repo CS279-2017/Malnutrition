75915aa42799ee2049760c29827a44580fe970c1
Bowen Jin <bowen.jin@vanderbilt.edu>
2017-02-26 18:49:16 -0600

    changed url again

--- malnutritionappserver-new/db/db.js
+++ malnutritionappserver-new/db/db.js
@@ -3,8 +3,7 @@ var RegistrationCollection = require("./registrationCollection.js")
 
 var MongoClient = require('mongodb').MongoClient;
 
-var url = process.env.MONGODB_URL
-    // || "mongodb://localhost:27017/nutririsk"
+var url = process.env.MONGO || "mongodb://localhost:27017/nutririsk"
 
 var registrationCollection;
 var userCollection;
